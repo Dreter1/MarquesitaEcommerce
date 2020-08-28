ad67cae1bcb1f18561815ff5890f5fc23f7995d4
Fernando Guerrero <dreter100@gmail.com>
2020-08-28 17:39:23 -0500

    Adding Controllers && Views

--- MarquesitaDashboards/Controllers/ProductController.cs
+++ MarquesitaDashboards/Controllers/ProductController.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MarquesitaDashboards.Controllers
 {
+    [Authorize]
     public class ProductController : Controller
     {
+        [AllowAnonymous]
         public IActionResult Index()
         {
             return View();
