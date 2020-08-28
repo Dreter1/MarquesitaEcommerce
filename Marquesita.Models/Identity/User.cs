e407e6fbd68e7e919c4a4bb717378041119a6ce0
Fernando Guerrero <dreter100@gmail.com>
2020-08-28 09:46:14 -0500

    Adding Identity Models

---
+++ Marquesita.Models/Identity/User.cs
@@ -0,0 +1,17 @@
+﻿//using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace Marquesita.Models.Identity
+{
+    public class User /*: IdentityUser*/
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public string MailToken { get; set; }
+        public string ResetPasswordToken { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public DateTime RegisterDate { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
