841a1d7a808074d82a2633acd99f3b37ad94753a
Fernando Guerrero <dreter100@gmail.com>
2020-08-28 20:18:10 -0500

    Revert "update gitignore adding views"
    This reverts commit a646cbebb59b33398d26d67bea30d9be0b6e5128.

--- MarquesitaEcommerce/Controllers/ShoppingCartController.cs
+++ MarquesitaEcommerce/Controllers/ShoppingCartController.cs
@@ -2,12 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MarquesitaEcommerce.Controllers
 {
-    [Authorize]
     public class ShoppingCartController : Controller
     {
         public IActionResult Index()
