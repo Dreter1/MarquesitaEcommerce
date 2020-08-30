fd2eabe450e67e26ca408184adc10f58e64c10f6
Fernando Guerrero <dreter100@gmail.com>
2020-08-29 21:26:44 -0500

    Adding AuthManagerService && LoginViewModel

---
+++ Marquesita.Infrastructure/Interfaces/IAuthManagerService.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+
+namespace Marquesita.Infrastructure.Interfaces
+{
+    public interface IAuthManagerService
+    {
+        Task<SignInResult> LoginAsync(string username, string password);
+        void LogOut();
+    }
+}
---
+++ Marquesita.Infrastructure/Services/AuthManagerService.cs
@@ -0,0 +1,27 @@
+﻿using Marquesita.Infrastructure.Interfaces;
+using Marquesita.Models.Identity;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+
+namespace Marquesita.Infrastructure.Services
+{
+    public class AuthManagerService : IAuthManagerService
+    {
+        private readonly SignInManager<User> _signInManager;
+
+        public AuthManagerService(SignInManager<User> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        public async Task<SignInResult> LoginAsync(string username, string password)
+        {
+            return await _signInManager.PasswordSignInAsync(username, password, false, false);
+        }
+
+        public void LogOut()
+        {
+            _signInManager.SignOutAsync().Wait();
+        }
+    }
+}
