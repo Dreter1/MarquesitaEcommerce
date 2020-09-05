5a3048db9ce8c27cbc40e9d7caa79a0e6ea17015
Fernando Guerrero <dreter100@gmail.com>
2020-09-04 23:56:35 -0500

    Adding Policy to Client

--- Marquesita.WebSite/Startup.cs
+++ Marquesita.WebSite/Startup.cs
@@ -218,7 +218,7 @@ namespace MarquesitaDashboards
                 });
 
                 // Client Policy
-                options.AddPolicy("ClientPolicy", policy =>
+                options.AddPolicy("Client", policy =>
                 {
                     policy.RequireClaim("Permission", "Compras");
                 });
