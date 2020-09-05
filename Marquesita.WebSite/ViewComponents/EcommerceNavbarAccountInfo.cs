d9f3abdf28adb7e62ae3f7f93d9d6572273d4042
Fernando Guerrero <dreter100@gmail.com>
2020-09-04 23:48:05 -0500

    Adding Eccomerce Components And Layout

---
+++ Marquesita.WebSite/ViewComponents/EcommerceNavbarAccountInfo.cs
@@ -0,0 +1,30 @@
+﻿using Marquesita.Infrastructure.Interfaces;
+using Marquesita.Models.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Marquesita.WebSite.ViewComponents
+{
+    public class EcommerceNavbarAccountInfo : ViewComponent
+    {
+        private readonly IUserManagerService _userManager;
+
+        public EcommerceNavbarAccountInfo(IUserManagerService userManager)
+        {
+            _userManager = userManager;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            if(User.Identity.Name != null)
+            {
+                var user = await _userManager.GetUserByNameAsync(User.Identity.Name);
+                return View(user);
+            }
+            return View(new User{ Id = null });
+
+        }
+    }
+}
