a23b2860cd048b7c547951db46db2d2d9e337400
Fernando Guerrero <dreter100@gmail.com>
2021-06-15 22:56:55 -0500

    updating comments model

--- Marquesita.Models/Business/Comments.cs
+++ Marquesita.Models/Business/Comments.cs
@@ -7,7 +7,7 @@ namespace Marquesita.Models.Business
         public Guid Id { get; set; }
         public string Text { get; set; }
         public DateTime Date { get; set; }
-        public Guid UserId { get; set; }
+        public string UserId { get; set; }
         public Guid ProductId { get; set; }
 
         public Product Product { get; set; }
